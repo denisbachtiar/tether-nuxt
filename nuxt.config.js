92305a5c3bb9a0e2873b3b690c6645129328ca63
Denis Bachtiar <denisbachtiar.db@gmail.com>
2020-06-21 16:07:48 +0700

    first commit

---
+++ nuxt.config.js
@@ -0,0 +1,72 @@
+
+export default {
+  mode: 'universal',
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&family=Poppins:wght@300;400;600&display=swap' }
+    ],
+    script: [
+      { src: 'https://code.iconify.design/1/1.0.6/iconify.min.js', body: true }
+    ]
+  },
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+  /*
+  ** Global CSS
+  */
+  css: [
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+    '~/plugins/vue-fragment'
+  ],
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+    '@nuxtjs/vuetify',
+    '@nuxtjs/moment'
+  ],
+  moment: {
+    /* module options */
+    defaultLocale: 'id',
+    locales: ['id']
+  },
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    ['nuxt-gmaps', {
+      key: 'AIzaSyDaYbdWKAZgyTRy_rFzr6UdRGNY_Emu3VE',
+      //you can use libraries: ['places']
+    }],
+    '@nuxtjs/axios',
+  ],
+  axios: {
+    // proxyHeaders: false
+  },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {
+    }
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,154 @@
+<template>
+  <fragment>
+  <p v-if="loading">LOADING.....</p>
+  <v-app v-else>
+  <swiper class="swiper">
+    <swiper-slide v-for="(item,i) in apiData.image" :key="i" :style="{'background-image': 'url(' + item.image + ')'}"></swiper-slide>
+  </swiper>
+  <v-container class="">
+    <div class="d-flex mt-2 align-center">
+      <div style="width: 50%; padding: 10px 10px 10px 0px; border-right: 1px solid rgb(245, 245, 245)">
+        <p class="mb-1">Tanggal Activity<span class="float-right bold">{{$moment(apiData.date).format('DD MMM Y')}}</span></p>
+        <p>Jam Activity<span class="float-right bold">{{$moment(apiData.date).format('HH:mm')}}</span></p>
+      </div>
+      <div class="d-flex align-center" style="padding: 10px 0px 10px 10px;">
+        <img :src="apiData.avatar" width="48" height="48" class="round">
+        <div class="ml-2">
+          <p class="pop xs sbold gray-text" v-text="compareDays == 0 
+          ? `Post: Hari ini, ${$moment(apiData.createdAt).format('HH:mm')}`
+          : compareDays == -1 
+          ? `Post: Kemarin, ${$moment(apiData.createdAt).format('HH:mm')}`
+          : `Post: ${$moment(apiData.createdAt).format('DD MMM Y, HH:mm')}`"></p>
+          <p class="bold">{{apiData.author_name}}</p>
+        </div>
+      </div>
+    </div>
+  </v-container>
+  <hr class="mt-2 mb-2">
+  <v-container class="d-flex">
+    <div class="">
+      <p class="gray-text"><span class="iconify" data-icon="ic:round-comment" data-inline="false"></span><span class="ml-2 pop sm">{{apiData.comment}}</span></p>
+    </div>
+    <div class="ml-auto">
+      <p class="gray-text"><span class="iconify " data-icon="fa-solid:share-alt" data-inline="false"></span><span class="ml-2 pop sm">{{apiData.share}}</span></p>
+    </div>
+  </v-container>
+  <hr class="mt-2 mb-2">
+  <v-container>
+    <p class="mb-2 sbold lg">{{apiData.title}}</p>
+    <p class="mb-6" v-html="contentBR"></p>
+    <!--MAPS-->
+    <p class="mb-2 sbold lg" v-text="apiData.city_id == 0 ? 'Link Terkait':'Lokasi'"></p>
+    <div style="background-color: #f2f2f2;
+    padding: 10px;" v-if="apiData.city_id == 0"><p><a :href="apiData.link">{{apiData.link}}</a></p></div>
+    <GMap
+    v-if="loading == false && apiData.city_id != 0"
+    ref="gMap"
+    language="en"
+    :center="{lat: latlng.lat, lng: latlng.lng}"
+    :zoom="14">
+      <GMapMarker
+        :position="{lat: latlng.lat, lng: latlng.lng}"
+      >
+      </GMapMarker>
+    </GMap>
+    <p v-if="apiData.city_id != 0">{{apiData.address}}</p>
+    <hr class="mt-9 mb-2">
+    <p class="sbold lg mb-2">Peserta <span class="float-right">{{apiData.user_joined}}/{{userJoined}} Slot</span></p>
+      <div class="d-flex align-center">
+        <div style="width: 46px" class="ml-2" v-for="(item, index) in apiData.listjoined" :key="index">
+          <img :src="item.user.avatar" height="45" width="100%" class="round"></img>
+          <p class="pop light text-gray-100 xs mt-0 text-center">{{item.user.username}}</p>
+        </div>
+        <a href="https://play.google.com/store/apps/details?id=com.b5g.tether" style="width: 46px;" class="ml-2"><span class="iconify joined" :data-icon="apiData.user_joined == 0 ? 'fa-solid:users':'bx:bxs-grid'" data-inline="false"></span><p class="light pop text-gray-100 xs mt-2 text-center" v-text="apiData.user_joined == 0 ? 'belum ada':'see all'"></p></a>  
+      </div>
+    <hr class="mt-3 mb-3">
+  </v-container>
+  <div style="background-color: rgb(245, 245, 245);" class="pt-2"></div>
+  <v-container style="background-color: #FFF7E5;" class="pt-4 pb-4">
+    <p class="sbold lg mb-2">Notes</p>
+    <p>{{apiData.note.note}}</p>
+  </v-container>
+  <div style="background-color: rgb(245, 245, 245);" class="pt-2"></div>
+  </v-app>
+</fragment>
+</template>
+
+<script>
+import { Swiper, SwiperSlide} from 'vue-awesome-swiper'
+import 'swiper/css/swiper.css'
+
+export default {
+  // async asyncData({ $axios }) {
+  //   const ip = await $axios.$get('http://prodapi.tether.co.id:8182/api/activity/Ajakan-main-Games-TopWar-Server-5551592067238803', {progress: true})
+
+  //   return { apiData: ip.data, loading: false }
+  // },
+  components: {
+    Swiper,
+    SwiperSlide
+  },
+  data () {
+      return {
+        loading: true,
+        apiData: {},
+      }
+    },
+    computed: {
+      contentBR: function() {
+        return String(this.apiData.content).replace(/\n/g, "<br/>")
+      },
+      latlng: function() {
+        var latlng = {lat: parseFloat(this.apiData.lat), lng: parseFloat(this.apiData.long)}
+        return latlng
+      },
+      compareDays: function() {
+        var deadline = new Date(this.apiData.createdAt).getTime();
+        var dateNow = new Date().getTime();
+        var day = 24*60*60*1000;
+        var compare = Math.round((Math.round(deadline)-Math.round(dateNow))/day)
+        return compare
+      },
+      userJoined: function() {
+        if (this.apiData.slot == 0) {
+          return 'unlimited'
+        }
+        else {
+          return this.apiData.slot
+        }
+      }
+    },
+    mounted() {
+      this.$axios.$get('http://prodapi.tether.co.id:8182/api/activity/Nobar-first-match-Liverpool-setelah-pandemi1591881147500')
+      .then(response => {
+        // console.log(response.data)
+        this.apiData = response.data
+        this.loading = false
+      }).catch(error => {
+        if (this.$axios.isCancel(error)) {
+          console.log('Request canceled', error)
+        } else {
+          // handle error
+        }
+      })
+    },
+}
+</script>
+
+<style lang="scss">
+.GMap__Wrapper {
+  height: 100px;
+}
+.swiper {
+  height: 190px;
+  width: 100%;
+
+  .swiper-slide {
+    display: flex;
+    background-color: white;
+    background-repeat: no-repeat;
+    background-position: center; 
+    background-size: cover;
+  }
+}
+</style>
